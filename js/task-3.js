535b7cfe3b90b9a8d79a96a3e6e9271ee8aacecc
migell1415 <154928249+migell1415@users.noreply.github.com>
2024-04-28 03:11:35 +0300

    Create task-1, 2, 3

---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+input.addEventListener('input', event => {
+    const inputValue = event.currentTarget.value.trim();
+    output.textContent = inputValue || 'Anonymous';
+});
